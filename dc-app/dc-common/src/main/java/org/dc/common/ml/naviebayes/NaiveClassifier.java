d3ec644abcad68f326de7fce5728534fe9856d4e
Digital Alchemist <developer.anwar@gmail.com>
2017-08-10 01:13:11 -0400

    Label Image tENsorFLOW Signed-off-by: Digital Alchemist <developer.anwar@gmail.com>

---
+++ dc-app/dc-common/src/main/java/org/dc/common/ml/naviebayes/NaiveClassifier.java
@@ -0,0 +1,17 @@
+package org.dc.common.ml.naviebayes;
+
+import java.util.List;
+
+public class NaiveClassifier implements Classifier {
+
+	@Override
+	public void trainClassifier(List<Human> humans) {
+		
+	}
+
+	@Override
+	public String classify(Human human) {
+		return null;
+	}
+
+}
